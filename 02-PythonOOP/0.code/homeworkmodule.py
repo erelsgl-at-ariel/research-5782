3a1d252e4bb1f047d633b02705493a4b8cb2759a
Erel Segal-Halevi <erelsgl@gmail.com>
2022-03-06 07:54:01 +0200

    homework module

---
+++ 02-PythonOOP/0.code/homeworkmodule.py
@@ -0,0 +1,20 @@
+"""
+An example module for the homework
+(hint 1: you can read this comment using: `homeworkmodule.__doc__`)
+"""
+
+def func1(param1: int, param2: dict[str,int]):
+    """
+    This function has two parameters
+    (hint 2: you can read this comment using: `homeworkmodule.func1.__doc__`)
+    (hint 3: you can read the annotations using: `homeworkmodule.func1.__annotations__`)
+    """
+    print("Hello")
+
+
+def func2()->list:
+    """
+    This function has no parameters and a return value.
+    """
+    return [1,2,3]
+
